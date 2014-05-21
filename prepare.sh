c6ddd63f4f5472445d8ed9e1e5600c11b866d3ae
Fabio Roger Manera <fabio.manera@madvertise.com>
2014-05-21 01:03:40 +0100

    script to download/update, resolve dependencies and copy networker jar to android.main project

---
+++ prepare.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+log()  { printf "%b\n" "$*"; }
+fail() { log "\nERROR: $*\n" ; exit 1 ; }
+
+abspath() { echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"; }
+
+clone() { 
+	git clone https://github.com/sneerteam/$1.git || fail "Failing cloning $1"
+}
+
+pull() {
+	git fetch
+	git rebase origin/master
+}
+
+update() {
+	if [[ -e ${ROOT}/$1 ]]; then
+		pushd ${ROOT}/$1 &> /dev/null
+		pull $1
+	else
+		pushd ${ROOT} &> /dev/null
+		clone $1
+	fi
+	popd &> /dev/null
+}
+
+if [[ "$1" == "" ]]; then
+	if [[ -e ../snapi ]]; then
+		ROOT=`pwd`/..
+	else
+		ROOT=`pwd`
+	fi
+else
+	ROOT=$1
+fi
+
+ROOT=`realpath ${ROOT}`
+
+log "Sneer projects root is ${ROOT}"
+
+[[ -e ${ROOT} ]] || mkdir -p ${ROOT}
+
+update "server"
+update "networker"
+update "snapi"
+update "android.main"
+update "android.chat"
+
+cd ${ROOT}/networker
+./gradlew clean
+./gradlew eclipse uploadArchives
+cp build/libs/networker-*.jar ../android.main/libs
+
+cd ${ROOT}/snapi
+./gradlew
